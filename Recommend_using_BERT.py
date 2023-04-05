233bd1de3af21c7ad308c3787ed4eca621e80223
kayaisacat <113482809+kayaisacat@users.noreply.github.com>
2023-04-05 16:30:10 -0400

    Recommend Dialogflow for py file

---
+++ Recommend_using_BERT.py
@@ -0,0 +1,85 @@
+# Library
+import pandas as pd
+import numpy as np
+import spacy
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from transformers import AutoTokenizer, AutoModel
+import torch
+from sklearn import feature_extraction
+from bs4 import BeautifulSoup
+import re
+import nltk
+import matplotlib.pyplot as plt
+from sklearn.metrics import calinski_harabasz_score, davies_bouldin_score
+from sklearn.metrics import silhouette_score
+
+
+# Load the data
+data = pd.read_csv('cleaned_data.csv')
+
+# Drop missing values
+data = data.dropna()
+# Drop duplicates
+data = data.drop_duplicates()
+
+# Take some sample of the data, since data is too large, it take too long to calculate
+np.random.seed(100)
+data = data.sample(5000)
+
+#--------------------Text Feature Enginnering------------------------------#
+
+# input prepreocess text
+nlp = spacy.load("en_core_web_sm")
+
+def preprocess(text):
+    doc = nlp(text)
+    return " ".join(token.lemma_ for token in doc if not token.is_stop and token.is_alpha)
+
+data["description_preprocessed"] = data["description"].apply(preprocess)
+
+# BERT feature extraction
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+model = AutoModel.from_pretrained("bert-base-uncased")
+
+def get_bert_embedding(text):
+    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True, max_length=512)
+    outputs = model(**inputs)
+    return outputs.last_hidden_state.mean(axis=1).detach().numpy()
+
+data["bert_embedding"] = data["description_preprocessed"].apply(get_bert_embedding)
+
+# Standardize BERT embeddings
+scaler = StandardScaler()
+bert_embeddings = np.vstack(data["bert_embedding"].values)
+scaled_bert_embeddings = scaler.fit_transform(bert_embeddings)
+
+# Cluster books using K-Means
+n_clusters = 20
+kmeans = KMeans(n_clusters=n_clusters)
+data["cluster"] = kmeans.fit_predict(scaled_bert_embeddings)
+
+# Recommendation function
+def recommend_book(input_text, top_n=1):
+    input_text_preprocessed = preprocess(input_text)
+    input_bert_embedding = get_bert_embedding(input_text_preprocessed)
+    input_scaled_bert_embedding = scaler.transform(input_bert_embedding)
+    
+    input_cluster = kmeans.predict(input_scaled_bert_embedding)
+    filtered_books = data[data["cluster"] == input_cluster[0]]
+
+    similarity_scores = []
+    for book_embedding in filtered_books["bert_embedding"]:
+        similarity_scores.append(torch.nn.functional.cosine_similarity(
+            torch.tensor(input_bert_embedding), torch.tensor(book_embedding))[0].item())
+    
+    best_match_index = np.argmax(similarity_scores)
+    
+    return filtered_books.iloc[best_match_index]
+
+# Test the recommendation system
+input_text = "A thrilling mystery novel with unexpected twists."
+recommended_book = recommend_book(input_text)
+print(recommended_book)
---
+++ Recommend_using_TFIDF.py
@@ -0,0 +1,61 @@
+# Library
+import pandas as pd
+import numpy as np
+import spacyRecommend_using_TFIDF.py
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from transformers import AutoTokenizer, AutoModel
+import torch
+from sklearn import feature_extraction
+from bs4 import BeautifulSoup
+import re
+import nltk
+import matplotlib.pyplot as plt
+from sklearn.metrics import calinski_harabasz_score, davies_bouldin_score
+from sklearn.metrics import silhouette_score
+
+
+# Load the data
+data = pd.read_csv('cleaned_data.csv')
+
+# Drop missing values
+data = data.dropna()
+# Drop duplicates
+data = data.drop_duplicates()
+
+# Take some sample of the data. 
+# Since data is too large, TF-IDF take too long to calculate
+np.random.seed(100)
+data = data.sample(10000)
+
+#--------------------Text Feature Enginnering------------------------------#
+
+# input prepreocess text
+nlp = spacy.load("en_core_web_sm")
+
+def preprocess(text):
+    doc = nlp(text)
+    return " ".join(token.lemma_ for token in doc if not token.is_stop and token.is_alpha)
+
+data["description_preprocessed"] = data["description"].apply(preprocess)
+
+# Create feature vectors for TF_IDF
+vectorizer = TfidfVectorizer()
+description_matrix = vectorizer.fit_transform(data["description_preprocessed"])
+
+# Recommendation function
+def recommend_book(input_text, top_n=1):
+    input_text_preprocessed = preprocess(input_text)
+    input_vector = vectorizer.transform([input_text_preprocessed])
+    
+    similarity_scores = cosine_similarity(input_vector, description_matrix)
+    best_match_index = np.argmax(similarity_scores)
+    
+    return data.iloc[best_match_index]
+
+# Test the recommendation system
+input_text = "A thrilling mystery novel with unexpected twists."
+recommended_book = recommend_book(input_text)
+print(recommended_book)
